e0591dabbc45f2aaf1221687c2d3233cad0941ff
r1k0 <erick@openchill.org>
2012-04-14 15:46:41 +0000

    timestamp debug server side

--- daemon/kicktood.pl
+++ daemon/kicktood.pl
@@ -6,6 +6,10 @@ use warnings;
 use HTTP::Daemon;
 use HTTP::Status;
 
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+# looks like 2012-04-14 15:43:56
+my $now = "%4d-%02d-%02d %02d:%02d:%02d\n",$year+1900,$mon+1,$mday,$hour,$min,$sec;
+
 my %conf = (
     port => 1331,
     bind_address => "0.0.0.0",
@@ -16,7 +20,7 @@ sub debug {
     my $msg = shift;
 
     if($conf{debug}) {
-        print "DEBUG: " . $msg . "\n";
+        print "DEBUG: $now " . $msg . "\n";
     }
 }
 
@@ -127,7 +131,8 @@ sub handle_request {
     } elsif($path eq "/get_profile") {
         $conn->send_file_response(get_profile_path($args->{mac}));
     } else {
-        debug("Sending 404");
+        # we don't need to respond it's just a server log update
+        # therefore sending http 404
         $conn->send_basic_header(404);
         $conn->send_crlf;
         print $conn "Unknown command";
@@ -146,7 +151,7 @@ sub main {
         debug("Accepted connection from " . $conn->peerhost() . ":" . $conn->peerport());
         handle_request($conn);
         $conn->close;
-        debug("Connection closed");
+        debug("Connection closed\n");
     }
 }
 
