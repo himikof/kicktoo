3b298ba6a10b008fa9e85243728c3f3a369cea37
r1k0 <erick@openchill.org>
2012-04-14 15:22:38 +0000

    fix -c IP -v when fetching profile_uri from kicktood.pl server

--- modules/server.sh
+++ modules/server.sh
@@ -23,6 +23,10 @@ server_send_request() {
 server_get_profile() {
     local profile_uri=$(server_send_request "get_profile_path" "mac=${mac_address}")
 
+    # when --verbose is passed the output gets appended before too which is bad
+    # so always make sure we stripped out the crap by always getting last field
+    profile_uri=$(echo ${profile_uri} | awk '{print $NF}')
+
     if [ -z "${profile_uri}" ]; then
         warn "error in response from server...could not retrieve profile URI"
         return 1
